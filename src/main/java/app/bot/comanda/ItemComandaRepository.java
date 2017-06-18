da233028c37700d54d09651b90f0ec0b22259e7a
brunoreili <brunoreili@gmail.com>
2017-06-18 13:40:33 -0300

    Teste Deleta Comanda Cliente

---
+++ src/main/java/app/bot/comanda/ItemComandaRepository.java
@@ -0,0 +1,12 @@
+package app.bot.comanda;
+
+import app.bot.cliente.Cliente;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ItemComandaRepository extends CrudRepository<ItemComanda, Integer>{
+
+    public Iterable<ItemComanda> findByCliente(Cliente cliente);
+
+    public void deleteByCliente(String first_name);
+    
+}
